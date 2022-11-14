bad5fea46ea9bb59453b52ddbc332e773c9abacb
rubenhernandez <rub3n.hernandez7@gmail.com>
2022-11-14 13:11:03 +0100

    Move test packages

--- src/test/java/com/kafka/Kafka/training/testContainer/KafkaTestContainersLiveTest.java
+++ src/test/java/com/kafka/Kafka/training/testContainer/KafkaTestContainersLiveTest.java
@@ -1,4 +1,4 @@
-package com.kafka.Kafka.training.streams.testContainer;
+package com.kafka.Kafka.training.testContainer;
 
 import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.admin.AdminClientConfig;
